4e739499e519144014b1c8004507fb48c42af9d8
zy <cot@zhangyi-desktop>
2024-05-16 14:26:52 +0800

    fix

--- scripts/deploy_vault_factory.sh
+++ scripts/deploy_vault_factory.sh
@@ -5,5 +5,5 @@
 dfx identity use default
 PINCIPAL=$(dfx identity get-principal)
 dfx deploy vault_factory --argument "(principal \"$PINCIPAL\", null)"
-gzip -f target/wasm32-unknown-unknown/release/vault-factory.wasm > target/wasm32-unknown-unknown/release/vault-factory.wasm.gz
-dfx canister install vault_factory --argument "(principal \"$PINCIPAL\", null)" --wasm target/wasm32-unknown-unknown/release/vault-factory.wasm.gz --mode=reinstall --yes
+# gzip -f target/wasm32-unknown-unknown/release/vault-factory.wasm > target/wasm32-unknown-unknown/release/vault-factory.wasm.gz
+# dfx canister install vault_factory --argument "(principal \"$PINCIPAL\", null)" --wasm target/wasm32-unknown-unknown/release/vault-factory.wasm.gz --mode=reinstall --yes
